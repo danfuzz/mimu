eeca5d0678839bba894a95fa96e822e89fa9736b
Dan Bornstein <danfuzz@milk.com>
2015-11-11 15:45:51 -0800

    First cut of Filter Explorer.

--- AUTHORS.md
+++ AUTHORS.md
@@ -21,6 +21,10 @@ The pink noise generator is based on
 and on the [gaussian random number generator](http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html)
 by Yu-Jie Lin, which is in turn based on other sources as cited.
 
+The filter code is based on the
+[Audio EQ Cookbook](http://www.musicdsp.org/files/Audio-EQ-Cookbook.txt)
+by Robert Bristow-Johnson.
+
 The basic script audio routing code was originally based on example code from
 Mozilla, though it has evolved significantly in the mean time. See
 <http://mdn.github.io/script-processor-node/>.
---
+++ filter-explorer/Piece.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2015 the Mimu Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
+"use strict";
+
+
+/**
+ * Filter generator, with adjustable center frequency and Q. What it filters
+ * is white noise.
+ *
+ * This is an implementation of a two-pole IIR band-pass filter, as described
+ * by Robert Bristow-Johnson in
+ * <http://www.musicdsp.org/files/Audio-EQ-Cookbook.txt>.
+ */
+class Piece {
+    constructor(sampleRate) {
+        // Base parameters
+
+        // Sample rate (samples per second).
+        this.sampleRate = sampleRate;
+
+        // Center frequency.
+        this._f0 = 440;
+
+        // Q.
+        this._q = 30.0;
+
+        // Amplitude of the output.
+        this._amp = 0.5;
+
+        // Derived values
+
+        // Previous two input samples. Subscripts are taken as negative
+        // indices.
+        this._x1 = 0;
+        this._x2 = 0;
+
+        // Previous two output samples. Subscripts are taken as negative
+        // indices.
+        this._y1 = 0;
+        this._y2 = 0;
+
+        // Coefficients for the filter. Named after what element they apply to.
+        this._x0Co = 0;
+        this._x1Co = 0;
+        this._x2Co = 0;
+        this._y1Co = 0;
+        this._y2Co = 0;
+
+        // Final setup.
+        this.calcFilter();
+    }
+
+    set amp(value) {
+        this._amp = value;
+    }
+
+    set f0(value) {
+        this._f0 = value;
+        this.calcFilter();
+    }
+
+    set q(value) {
+        if (value <= 0.0001) {
+            value = 0.0001;
+        }
+
+        this._q = value;
+        this.calcFilter();
+    }
+
+    // Calculate the filter parameters.
+    calcFilter() {
+        var w0 = 2 * Math.PI * this._f0 / this.sampleRate;
+        var alpha = Math.sin(w0) / (this._q * 2);
+
+        var b0 = this._q * alpha;
+        var b1 = 0;
+        var b2 = -this._q * alpha;
+        var a0 = 1 + alpha;
+        var a1 = -2 * Math.cos(w0);
+        var a2 = 1 - alpha;
+
+        this._x0Co = b0 / a0;
+        this._x1Co = b1 / a0;
+        this._x2Co = b2 / a0;
+        this._y1Co = -a1 / a0;
+        this._y2Co = -a2 / a0;
+    }
+
+    // Perform one iteration of generation, returning a single sample.
+    nextSample() {
+        var x1 = this._x1;
+        var x2 = this._x2;
+        var y1 = this._y1;
+        var y2 = this._y2;
+
+        var x0 = Math.random();  // Current input sample.
+        var y0 = (this._x0Co * x0) + (this._x1Co * x1) + (this._x2Co * x2) +
+            (this._y1Co * y1) + (this._y2Co * y2);
+
+        this._x2 = this._x1;
+        this._x1 = x0;
+        this._y2 = this._y1;
+        this._y1 = y0;;
+
+        return y0 * this._amp;
+    }
+}
